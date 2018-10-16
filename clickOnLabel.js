8904df0b1993a76f74afaad972122f9edb40c625
Sergey Sadovikov <nexxerr@gmail.com>
2018-10-16 12:00:02 +0300

    curp

--- clickOnLabel.js
+++ clickOnLabel.js
@@ -1,6 +1,7 @@
 	// Простая конструкция, с помощью которой
 	// даем возможность не целиться в чекбокс,
 	// но и нажать на лейбл
+	// Не забыть сделать на лейбл cursor: pointer;
 	$('label').on('click', function() {
 		var labelFor = $(this).attr('for');
 		$('input[name="'+labelFor+'"]').click();
